00e9eeac8fe219d150ff58a218ff5f91871385b4
Cassio <cassioate@gmail.com>
2021-05-13 14:16:43 -0300

    Iinicio da aplicação (APP/routes/Server)

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+// import { Router } from 'express'
+
+const { Router } = require('express');
+
+const routes = new Router();
+
+routes.get('/', (req, res) => {
+    return res.json({message: "opa"})
+})
+
+module.exports = routes;
---
+++ src/server.js
@@ -0,0 +1,4 @@
+// import App from './app'
+const App = require('./app')
+
+App.listen(3333);
